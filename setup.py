527d8ab4e42faf0ec63f3ff61ed7dd4911b8fba3
anjianshi <anjianshi@gmail.com>
2015-07-31 11:28:13 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ except ImportError:
 
 setup(
     name='API-libs',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/anjianshi/api-libs',
     license='MIT',
     author='anjianshi',
     author_email='anjianshi@gmail.com',
     description="一套灵活的组件，帮助你快速搭建起一个 API 系统",
-    packages=['api_libs'],
+    packages=['api_libs', "api_libs.adapters", "api_libs.parameters"],
     zip_safe=False,
     platforms='any',
     keywords=['api', 'tornado'],
