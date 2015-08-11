6a089b1063e89a9a8d19e56ff54231f60ba77abe
anjianshi <anjianshi@gmail.com>
2015-08-12 01:13:39 +0800

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='API-libs',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/anjianshi/api-libs',
     license='MIT',
     author='anjianshi',
