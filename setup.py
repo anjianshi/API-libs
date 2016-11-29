5725afc4770380451c4d8e19e2ca1b33525bec18
anjianshi <anjianshi@gmail.com>
2016-11-29 21:06:30 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='API-libs',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/anjianshi/api-libs',
     license='MIT',
     author='anjianshi',
